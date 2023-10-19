850f8ec313a8dd7dda0994073ec1abaefa49f5d2
Karesz0411 <karesz04111@gmail.com>
2023-10-19 19:51:25 +0200

    Abstract and UserDo created

---
+++ models/dos/abstract_do.js
@@ -0,0 +1,5 @@
+let AbstractDo = {
+    created_at: null,    // Date of account creation
+    updated_at: null,    // Date of the last update
+    is_active: false     // Whether the user's account is active
+};
