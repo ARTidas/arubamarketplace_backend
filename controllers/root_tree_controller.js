7f668b7eb2e0fc9c9cc1bae8a1a3a94a408db73d
trissz <triszten25@gmail.com>
2023-10-28 11:16:59 +0200

    tree

--- controllers/root_tree_controller.js
+++ controllers/root_tree_controller.js
@@ -1,31 +1,31 @@
 class TreeNode {
-    constructor(id, path) {
+    constructor(id, path, textInput = "") {
       this.id = id;
       this.path = path;
+      this.textInput = textInput;
       this.children = [];
     }
   
     addChild(childNode) {
       this.children.push(childNode);
     }
-
+  
     popChild(childId) {
-        const index = this.children.findIndex(child => child.id === childId);
-        if (index !== -1) {
-          return this.children.splice(index, 1)[0];
-        }
-        return null; // Child with the specified ID not found
+      const index = this.children.findIndex(child => child.id === childId);
+      if (index !== -1) {
+        return this.children.splice(index, 1)[0];
       }
+      return null; // Child with the specified ID not found
+    }
   }
-
-  //Function to build a new TreeNode object
+  
   function buildTree(data) {
     const dataMap = new Map();
-    const root = new TreeNode(0, { name: "https://localhost:8080/hello" });
+    const root = new TreeNode(0, { name: "https://localhost:8080/api/hello" }, "Root text");
   
     data.forEach(nodeData => {
-      const { id, name, parentId } = nodeData;
-      const newNode = new TreeNode(id, { name });
+      const { id, name, parentId, textInput } = nodeData; // Include textInput in object destructuring
+      const newNode = new TreeNode(id, { name }, textInput);
       dataMap.set(id, newNode);
   
       if (parentId === null) {
@@ -40,30 +40,28 @@ class TreeNode {
   
     return root;
   }
-
-  /* Push back the paths */
-
+  
   const data = [
-    { id: 1, name: "https://localhost:8080/hello/company-website", parentId: null },
-    { id: 2, name: "https://localhost:8080/hello/company-website/cluster-yes", parentId: 1 },
-    { id: 3, name: "https://localhost:8080/hello/company-website/cluster-yes/domain-yes", parentId: 2 },
-    { id: 4, name: "https://localhost:8080/hello/company-website/cluster-yes/domain-no", parentId: 2 },
-    { id: 5, name: "https://localhost:8080/hello/company-website/cluster-no", parentId: 1 },
-    { id: 6, name: "https://localhost:8080/hello/company-website/cluster-no/another-provider-yes", parentId: 2 },
-    { id: 7, name: "https://localhost:8080/hello/company-website/cluster-no/another-provider-no", parentId: 2 },
-    { id: 8, name: "https://localhost:8080/hello/wordpress-shop", parentId: null },
-    { id: 9, name: "https://localhost:8080/hello/kubernetes-education", parentId: null },
-    { id: 10, name: "https://localhost:8080/hello/marketing", parentId: null },
-    { id: 11, name: "https://localhost:8080/hello/cost-optimization", parentId: null },
-    { id: 12, name: "https://localhost:8080/hello/publish-application", parentId: null },
-    { id: 13, name: "https://localhost:8080/hello/organize-hackathon", parentId: null }
+    { id: 1, name: "https://localhost:8080/api/hello/company-website", parentId: null, textInput: "<p>Text for Node 1</p>" },
+    { id: 2, name: "https://localhost:8080/api/hello/company-website/cluster-yes", parentId: 1, textInput: "<p>Text for Node 2</p>" },
+    { id: 3, name: "https://localhost:8080/api/hello/company-website/cluster-yes/domain-yes", parentId: 2, textInput: "<p>Text for Node 3</p>" },
+    { id: 4, name: "https://localhost:8080/api/hello/company-website/cluster-yes/domain-no", parentId: 2, textInput: "<p>Text for Node 4</p>" },
+    { id: 5, name: "https://localhost:8080/api/hello/company-website/cluster-no", parentId: 1, textInput: "<p>Text for Node 5</p>" },
+    { id: 6, name: "https://localhost:8080/api/hello/company-website/cluster-no/another-provider-yes", parentId: 2, textInput: "<p>Text for Node 6</p>" },
+    { id: 7, name: "https://localhost:8080/api/hello/company-website/cluster-no/another-provider-no", parentId: 2, textInput: "<p>Text for Node 7</p>" },
+    { id: 8, name: "https://localhost:8080/api/hello/wordpress-shop", parentId: null, textInput: "<p>Text for Node 8</p>" },
+    { id: 9, name: "https://localhost:8080/api/hello/kubernetes-education", parentId: null, textInput: "<p>Text for Node 9</p>" },
+    { id: 10, name: "https://localhost:8080/api/hello/marketing", parentId: null, textInput: "<p>Text for Node 10</p>" },
+    { id: 11, name: "https://localhost:8080/api/hello/cost-optimization", parentId: null, textInput: "<p>Text for Node 11</p>" },
+    { id: 12, name: "https://localhost:8080/api/hello/publish-application", parentId: null, textInput: "<p>Text for Node 12</p>" },
+    { id: 13, name: "https://localhost:8080/api/hello/organize-hackathon", parentId: null, textInput: "<p>Text for Node 13</p>" }
   ];
-
+  
   const tree = buildTree(data);
   
   // Function to print the tree
   function printTree(node, level = 0) {
-    console.log(' '.repeat(level * 2) + `ID: ${node.id}, Name: ${node.path.name}`);
+    console.log(' '.repeat(level * 2) + `ID: ${node.id}, Name: ${node.path.name}, Text Input: ${node.textInput}`);
     for (const child of node.children) {
       printTree(child, level + 1);
     }
@@ -72,4 +70,3 @@ class TreeNode {
   // Print the tree
   printTree(tree);
   
-  module.exports = {TreeNode, buildTree, printTree};
