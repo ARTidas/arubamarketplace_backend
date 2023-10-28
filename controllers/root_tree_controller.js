c2405215d4dcaaa31892eae8b30a673380580e74
trissz <triszten25@gmail.com>
2023-10-28 10:26:13 +0200

    tree building

--- controllers/root_tree_controller.js
+++ controllers/root_tree_controller.js
@@ -21,7 +21,7 @@ class TreeNode {
   //Function to build a new TreeNode object
   function buildTree(data) {
     const dataMap = new Map();
-    const root = new TreeNode(0, { name: "Root" });
+    const root = new TreeNode(0, { name: "https://localhost:8080/hello" });
   
     data.forEach(nodeData => {
       const { id, name, parentId } = nodeData;
@@ -46,18 +46,22 @@ class TreeNode {
   const data = [
     { id: 1, name: "Node 1", parentId: null },
     { id: 2, name: "Node 2", parentId: 1 },
-    { id: 3, name: "Node 3", parentId: 1 }
+    { id: 3, name: "Node 3", parentId: 1 },
+    { id: 4, name: "Node 4", parentId: null },
+    { id: 5, name: "Node 5", parentId: 1 }
   ];
+
+  const tree = buildTree(data);
   
   // Function to print the tree
   function printTree(node, level = 0) {
-    console.log(' '.repeat(level * 2) + `ID: ${node.id}, Name: ${node.data.name}`);
+    console.log(' '.repeat(level * 2) + `ID: ${node.id}, Name: ${node.path.name}`);
     for (const child of node.children) {
       printTree(child, level + 1);
     }
   }
   
   // Print the tree
-  printTree(root);
+  printTree(tree);
   
   module.exports = {TreeNode, buildTree, printTree};
