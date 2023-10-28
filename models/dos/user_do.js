16e73537380af0e33c689c7c9ae914d9f5362a69
trissz <triszten25@gmail.com>
2023-10-29 01:39:51 +0200

    Update user_do.js

--- models/dos/user_do.js
+++ models/dos/user_do.js
@@ -1,16 +1,10 @@
 const AbstractDo = require("./abstract_do");
 
 class userDo extends AbstractDo {
-    constructor(id, email, password_hash, is_active, created_at, is_admin, owned_products_id, balance, updated_at) {
-        this.id = id;
+    constructor(email, password_hash, owned_products_id) {
         this.email = email;
         this.password_hash = password_hash;
-        this.is_active = is_active;
-        this.created_at = created_at;
-        this.is_admin = is_admin;
         this.owned_products_id = owned_products_id;
-        this.balance = balance;
-        this.updated_at = updated_at;
     }
 };
 
