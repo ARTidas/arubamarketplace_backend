ee099f206e0191daa6aea2bd40395566256efd0a
trissz <triszten25@gmail.com>
2023-10-28 12:39:41 +0200

    tree fix

--- app.js
+++ app.js
@@ -27,18 +27,18 @@ app.get('/api/hello/:id', async (req, res) => {
 
   const data = [
     { id: 1, name: "https://localhost:8080/api/hello/1", url_part: "company-website", parentId: null, textInput: "<p>Text for Node 1</p>" },
-    { id: 2, name: "https://localhost:8080/api/hello/company-website/cluster-yes", url_part: "company-website/cluster-yes", parentId: 1, textInput: "<p>Text for Node 2</p>" },
-    { id: 3, name: "https://localhost:8080/api/hello/company-website/cluster-yes/domain-yes", url_part: "company-website/cluster-yes/domain-yes", parentId: 2, textInput: "<p>Text for Node 3</p>" },
-    { id: 4, name: "https://localhost:8080/api/hello/company-website/cluster-yes/domain-no", url_part: "company-website/cluster-yes/domain-no", parentId: 2, textInput: "<p>Text for Node 4</p>" },
-    { id: 5, name: "https://localhost:8080/api/hello/company-website/cluster-no", url_part: "company-website/cluster-no", parentId: 1, textInput: "<p>Text for Node 5</p>" },
-    { id: 6, name: "https://localhost:8080/api/hello/company-website/cluster-no/another-provider-yes", url_part: "company-website/cluster-no/another-provider-yes", parentId: 2, textInput: "<p>Text for Node 6</p>" },
-    { id: 7, name: "https://localhost:8080/api/hello/company-website/cluster-no/another-provider-no", url_part: "company-website/cluster-no/another-provider-no", parentId: 2, textInput: "<p>Text for Node 7</p>" },
-    { id: 8, name: "https://localhost:8080/api/hello/wordpress-shop", url_part: "wordpress-shop", parentId: null, textInput: "<p>Text for Node 8</p>" },
-    { id: 9, name: "https://localhost:8080/api/hello/kubernetes-education", url_part: "kubernetes-education", parentId: null, textInput: "<p>Text for Node 9</p>" },
-    { id: 10, name: "https://localhost:8080/api/hello/marketing", url_part: "marketing", parentId: null, textInput: "<p>Text for Node 10</p>" },
-    { id: 11, name: "https://localhost:8080/api/hello/cost-optimization", url_part: "cost-optimization", parentId: null, textInput: "<p>Text for Node 11</p>" },
-    { id: 12, name: "https://localhost:8080/api/hello/publish-application", url_part: "publish-application", parentId: null, textInput: "<p>Text for Node 12</p>" },
-    { id: 13, name: "https://localhost:8080/api/hello/organize-hackathon", url_part: "organize-hackathon", parentId: null, textInput: "<p>Text for Node 13</p>" }
+    { id: 2, name: "https://localhost:8080/api/hello/2", url_part: "company-website/cluster-yes", parentId: 1, textInput: "<p>Text for Node 2</p>" },
+    { id: 3, name: "https://localhost:8080/api/hello/3", url_part: "company-website/cluster-yes/domain-yes", parentId: 2, textInput: "<p>Text for Node 3</p>" },
+    { id: 4, name: "https://localhost:8080/api/hello/4", url_part: "company-website/cluster-yes/domain-no", parentId: 2, textInput: "<p>Text for Node 4</p>" },
+    { id: 5, name: "https://localhost:8080/api/hello/5", url_part: "company-website/cluster-no", parentId: 1, textInput: "<p>Text for Node 5</p>" },
+    { id: 6, name: "https://localhost:8080/api/hello/6", url_part: "company-website/cluster-no/another-provider-yes", parentId: 2, textInput: "<p>Text for Node 6</p>" },
+    { id: 7, name: "https://localhost:8080/api/hello/7", url_part: "company-website/cluster-no/another-provider-no", parentId: 2, textInput: "<p>Text for Node 7</p>" },
+    { id: 8, name: "https://localhost:8080/api/hello/8", url_part: "wordpress-shop", parentId: null, textInput: "<p>Text for Node 8</p>" },
+    { id: 9, name: "https://localhost:8080/api/hello/9", url_part: "kubernetes-education", parentId: null, textInput: "<p>Text for Node 9</p>" },
+    { id: 10, name: "https://localhost:8080/api/hello/10", url_part: "marketing", parentId: null, textInput: "<p>Text for Node 10</p>" },
+    { id: 11, name: "https://localhost:8080/api/hello/11", url_part: "cost-optimization", parentId: null, textInput: "<p>Text for Node 11</p>" },
+    { id: 12, name: "https://localhost:8080/api/hello/12", url_part: "publish-application", parentId: null, textInput: "<p>Text for Node 12</p>" },
+    { id: 13, name: "https://localhost:8080/api/hello/13", url_part: "organize-hackathon", parentId: null, textInput: "<p>Text for Node 13</p>" }
   ];
 
   const tree = buildTree(data);
