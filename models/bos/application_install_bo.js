5a35537024f4a5f6ef9f98d3732c14bf201fc382
Karesz0411 <91952234+Karesz0411@users.noreply.github.com>
2023-10-28 15:08:18 +0200

    bo changed

--- models/bos/application_install_bo.js
+++ models/bos/application_install_bo.js
@@ -1,19 +1,19 @@
-const applicationInstallDao = require('../daos/application_install_dao');
+const appInstallDao = require('../daos/application_install_dao');
 
-function handleUserClick(nodeId, decision) {
-  if (!nodeId) {
-    throw new Error('Hiányzó node ID.');
+function handleUserClick(nodeId, decision, userId, status, version) {
+  if (!nodeId || !userId || decision !== 'Yes' && decision !== 'No') {
+    throw new Error('Érvénytelen adatok vagy döntés.');
   }
 
-  // Validáció és üzleti logika
-  if (decision === 'Yes') {
-    // Döntés kezelése, ha 'Yes'
-    // Példa: applicationInstallDao.createInstallation(name, description);
-  } else if (decision === 'No') {
-    // Döntés kezelése, ha 'No'
-    // Példa: applicationInstallDao.createInstallation(name, description);
+  // Döntés kezelése
+  // Példa: Az alábbi sor a függvénytől függően változhat
+  const result = appInstallDao.insertClickToDatabase(userId, nodeId, status, version);
+
+  if (result) {
+    // Sikeres adatbázisba írás
+    return result;
   } else {
-    throw new Error('Érvénytelen döntés.');
+    throw new Error('Hiba az adatbázisba írás során.');
   }
 }
 
