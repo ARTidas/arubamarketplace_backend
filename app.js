c817e2ecae1a1436dcc01037c4db6f48bd827eb7
trissz <triszten25@gmail.com>
2023-10-28 14:39:12 +0200

    app.js change

--- app.js
+++ app.js
@@ -28,20 +28,37 @@ app.get('/api/hello/:id', async (req, res) => {
   const { id } = req.params; // Extract the node ID from the URL
 
   const data = [
-    { id: 1, name: "https://localhost:8080/api/hello/1", url_part: "company-website", parentId: null, textInput: "<p>Text for Node 1</p>" },
-    { id: 2, name: "https://localhost:8080/api/hello/2", url_part: "company-website/cluster-yes", parentId: 1, textInput: "<p>Text for Node 2</p>" },
-    { id: 3, name: "https://localhost:8080/api/hello/3", url_part: "company-website/cluster-yes/domain-yes", parentId: 2, textInput: "<p>Text for Node 3</p>" },
-    { id: 4, name: "https://localhost:8080/api/hello/4", url_part: "company-website/cluster-yes/domain-no", parentId: 2, textInput: "<p>Text for Node 4</p>" },
-    { id: 5, name: "https://localhost:8080/api/hello/5", url_part: "company-website/cluster-no", parentId: 1, textInput: "<p>Text for Node 5</p>" },
-    { id: 6, name: "https://localhost:8080/api/hello/6", url_part: "company-website/cluster-no/another-provider-yes", parentId: 2, textInput: "<p>Text for Node 6</p>" },
-    { id: 7, name: "https://localhost:8080/api/hello/7", url_part: "company-website/cluster-no/another-provider-no", parentId: 2, textInput: "<p>Text for Node 7</p>" },
-    { id: 8, name: "https://localhost:8080/api/hello/8", url_part: "wordpress-shop", parentId: null, textInput: "<p>Text for Node 8</p>" },
-    { id: 9, name: "https://localhost:8080/api/hello/9", url_part: "kubernetes-education", parentId: null, textInput: "<p>Text for Node 9</p>" },
-    { id: 10, name: "https://localhost:8080/api/hello/10", url_part: "marketing", parentId: null, textInput: "<p>Text for Node 10</p>" },
-    { id: 11, name: "https://localhost:8080/api/hello/11", url_part: "cost-optimization", parentId: null, textInput: "<p>Text for Node 11</p>" },
-    { id: 12, name: "https://localhost:8080/api/hello/12", url_part: "publish-application", parentId: null, textInput: "<p>Text for Node 12</p>" },
-    { id: 13, name: "https://localhost:8080/api/hello/13", url_part: "organize-hackathon", parentId: null, textInput: "<p>Text for Node 13</p>" }
+    { id: 1, name: "https://localhost:8080/api/hello/1", url_part: "company-website", parentId: null, textInput: "Do you have an Aruba Kubernetes cluster for your project?",
+    decisions: [
+            { decision: "Yes", children_id: 2 },
+            { decision: "No", children_id: 5 }
+        ]
+    },
+    { id: 2, name: "https://localhost:8080/api/hello/2", url_part: "company-website/cluster-yes", parentId: 1, textInput: "Do you have a domain name?",
+    decisions: [
+            { decision: "Yes", children_id: 3 },
+            { decision: "No", children_id: 4 }
+        ]
+    },
+    { id: 3, name: "https://localhost:8080/api/hello/3", url_part: "company-website/cluster-yes/domain-yes", parentId: 2, textInput: "I am ready to install everything for you. Would you like to pay with credit card or crypto?", decisions: [] },
+    { id: 4, name: "https://localhost:8080/api/hello/4", url_part: "company-website/cluster-yes/domain-no", parentId: 2, textInput: "You are able to get a domain name here.", decisions: [] },
+    { id: 5, name: "https://localhost:8080/api/hello/5", url_part: "company-website/cluster-no", parentId: 1, textInput: "Do you have a Kubernetes cluster at another provider?",
+    decisions: [
+        { decision: "Yes", children_id: 6 },
+        { decision: "No", children_id: 7 }
+    ]
+    },
+    { id: 6, name: "https://localhost:8080/api/hello/6", url_part: "company-website/cluster-no/another-provider-yes", parentId: 2, textInput: "Would you like to compute which is more beneficial?", decisions: [] },
+    { id: 7, name: "https://localhost:8080/api/hello/7", url_part: "company-website/cluster-no/another-provider-no", parentId: 2, textInput: "", decisions: [] },
+    { id: 8, name: "https://localhost:8080/api/hello/8", url_part: "wordpress-shop", parentId: null, textInput: "", decisions: [] },
+    { id: 9, name: "https://localhost:8080/api/hello/9", url_part: "kubernetes-education", parentId: null, textInput: "", decisions: [] },
+    { id: 10, name: "https://localhost:8080/api/hello/10", url_part: "marketing", parentId: null, textInput: "", decisions: [] },
+    { id: 11, name: "https://localhost:8080/api/hello/11", url_part: "cost-optimization", parentId: null, textInput: "", decisions: [] },
+    { id: 12, name: "https://localhost:8080/api/hello/12", url_part: "publish-application", parentId: null, textInput: "", decisions: [] },
+    { id: 13, name: "https://localhost:8080/api/hello/13", url_part: "organize-hackathon", parentId: null, textInput: "", decisions: [] }
   ];
+
+  console.log(data);
 
   const tree = buildTree(data);
 
@@ -83,11 +100,12 @@ function buildTree(data) {
   const dataMap = new Map();
 
   class TreeNode {
-    constructor(id, path,url_part = "", textInput = "") {
+    constructor(id, path,url_part = "", textInput = "", decisions) {
       this.id = id;
       this.path = path;
       this.url_part = url_part;
       this.textInput = textInput;
+      this.decisions = decisions;
       this.children = [];
     }
   
@@ -104,11 +122,21 @@ function buildTree(data) {
     }
   }
 
-  const root = new TreeNode(0, { name: "https://localhost:8080/api/hello/0" }, "", "Root text");
+  const root = new TreeNode(0, { name: "https://localhost:8080/api/hello/0" }, "", "Hello, I am Amedeo, the Aruba AI assistant. What technological challenges can I help with?",
+  [
+      { decision: "Hello there Amedeo, I would like to have a company website", children_id: 1 },
+      { decision: "I would like to have a Wordpress shop", children_id: 1 },
+      { decision: "I would like to learn more about Kubernetes", children_id: 1 },
+      { decision: "I would like to increase my sales number", children_id: 1 },
+      { decision: "I would like to optimize my IT infrastructure cost", children_id: 1 },
+      { decision: "I would like to publish an application", children_id: 1 },
+      { decision: "I would like to organize a Kubernetes Hackathon", children_id: 1 }
+  ]
+);
 
   data.forEach(nodeData => {
-    const { id, name, url_part, parentId, textInput } = nodeData; // Include textInput in object destructuring
-    const newNode = new TreeNode(id, { name }, url_part, textInput);
+    const { id, name, url_part, parentId, textInput, decisions } = nodeData; // Include textInput in object destructuring
+    const newNode = new TreeNode(id, { name }, url_part, textInput, decisions);
     dataMap.set(id, newNode);
 
     if (parentId === null) {
