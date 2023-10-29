24d170b8c9fee4c6821e17a0af53f0d848076b05
Karesz0411 <91952234+Karesz0411@users.noreply.github.com>
2023-10-29 02:28:58 +0100

    Update user_login_controller.js

--- controllers/user_login_controller.js
+++ controllers/user_login_controller.js
@@ -4,25 +4,20 @@ const router = express.Router();
 const userBo = require('../models/bos/user_bo'); // Import the user business object
 
 // Define a route for user login
-router.post('/login', async (req, res) => {
+router.post('/log', async (req, res) => {
     const { email, password } = req.body;
 
     console.log("Into the backlands");
     console.log(email);
     console.log(password);
 
-    try {
-        const user = await userBo.login(email, password);
-
-        if (user) {
-            res.json({ message: 'Login successful' });
-        } else {
-            res.status(401).json({ error: 'Invalid email or password' });
-        }
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+     // Ellenőrizd az email és jelszó egyezését
+  const result = await userBo.comparePassword(email, password);
+  if (result) {
+    res.status(200).json({ message: 'Sikeres bejelentkezés' });
+  } else {
+    res.status(401).json({ message: 'Sikertelen bejelentkezés' });
+  }
 });
 
 module.exports = router;
